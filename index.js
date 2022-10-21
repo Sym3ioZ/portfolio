e2377d5b9488d52520445e1b64910681310dd8d0
Sym3ioZ <tortevois.benoit@gmail.com>
2022-10-21 11:32:20 +0200

    navmenu OK. Visual and script.

--- index.js
+++ index.js
@@ -2,19 +2,82 @@
 
 const prezBlock = document.getElementById('prezBlock')
 const prezTab = document.getElementById('prezTab')
-
 const techsBlock = document.getElementById('techsBlock')
 const techsTab = document.getElementById('techsTab')
+const projectsBlock = document.getElementById('projectsBlock')
+const projectsTab = document.getElementById('projectsTab')
+const resumeBlock = document.getElementById('resumeBlock')
+const resumeTab = document.getElementById('resumeTab')
+const contactBlock = document.getElementById('contactBlock')
+const contactTab = document.getElementById('contactTab')
 
 prezTab.addEventListener('click', displayPrez)
 techsTab.addEventListener('click', displayTechs)
+projectsTab.addEventListener('click', displayProjects)
+resumeTab.addEventListener('click', displayResume)
+contactTab.addEventListener('click', displayContact)
 
 function displayPrez() {
     prezBlock.setAttribute('style', 'display: block')
+    prezTab.classList.add('selected')
     techsBlock.setAttribute('style', 'display: none')
+    techsTab.classList.remove('selected')
+    projectsBlock.setAttribute('style', 'display: none')
+    projectsTab.classList.remove('selected')
+    resumeBlock.setAttribute('style', 'display: none')
+    resumeTab.classList.remove('selected')
+    contactBlock.setAttribute('style', 'display: none')
+    contactTab.classList.remove('selected')
 }
 
 function displayTechs() {
     prezBlock.setAttribute('style', 'display: none')
+    prezTab.classList.remove('selected')
     techsBlock.setAttribute('style', 'display: block')
+    techsTab.classList.add('selected')
+    projectsBlock.setAttribute('style', 'display: none')
+    projectsTab.classList.remove('selected')
+    resumeBlock.setAttribute('style', 'display: none')
+    resumeTab.classList.remove('selected')
+    contactBlock.setAttribute('style', 'display: none')
+    contactTab.classList.remove('selected')
+}
+
+function displayProjects() {
+    prezBlock.setAttribute('style', 'display: none')
+    prezTab.classList.remove('selected')
+    techsBlock.setAttribute('style', 'display: none')
+    techsTab.classList.remove('selected')
+    projectsBlock.setAttribute('style', 'display: block')
+    projectsTab.classList.add('selected')
+    resumeBlock.setAttribute('style', 'display: none')
+    resumeTab.classList.remove('selected')
+    contactBlock.setAttribute('style', 'display: none')
+    contactTab.classList.remove('selected')
+}
+
+function displayResume() {
+    prezBlock.setAttribute('style', 'display: none')
+    prezTab.classList.remove('selected')
+    techsBlock.setAttribute('style', 'display: none')
+    techsTab.classList.remove('selected')
+    projectsBlock.setAttribute('style', 'display: none')
+    projectsTab.classList.remove('selected')
+    resumeBlock.setAttribute('style', 'display: block')
+    resumeTab.classList.add('selected')
+    contactBlock.setAttribute('style', 'display: none')
+    contactTab.classList.remove('selected')
+}
+
+function displayContact() {
+    prezBlock.setAttribute('style', 'display: none')
+    prezTab.classList.remove('selected')
+    techsBlock.setAttribute('style', 'display: none')
+    techsTab.classList.remove('selected')
+    projectsBlock.setAttribute('style', 'display: none')
+    projectsTab.classList.remove('selected')
+    resumeBlock.setAttribute('style', 'display: none')
+    resumeTab.classList.remove('selected')
+    contactBlock.setAttribute('style', 'display: block')
+    contactTab.classList.add('selected')
 }
